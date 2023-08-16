c65fcc1ae08f659c3cf09e031ea91afb696646a6
Ravil Mamyshev <ravilmamyshev@yandex.ru>
2023-08-16 16:37:46 +0400

    Added SQLQuery

---
+++ CreateSQLQuery.sql
@@ -0,0 +1,9 @@
+﻿SELECT 
+	P.name AS Product,
+	C.name AS Category	
+FROM dbo.Product P
+	LEFT OUTER JOIN		
+		(dbo.ProductCategory AS PC
+			INNER JOIN dbo.Category C
+				ON C.id = PC.categoryId)
+		ON P.id = PC.productId;
